09e12e4ab57a70e8db8449168d1ea3684b6b2488
moorels <sales@micropls.com>
2021-12-25 12:14:42 +1100

    new

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,22 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <header>
+        <h1>Redwood Blog About</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>Home</main>
+    </>
+  )
+}
+
+export default AboutPage
